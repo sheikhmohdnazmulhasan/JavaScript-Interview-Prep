e0fd75ecb6c7d6d5c2a3c49a3b4b059d14798edd
mdnazmul42726 <nazmulofficial@outlook.com>
2023-12-30 16:36:11 +0600

    Function Declaration

---
+++ index.js
@@ -0,0 +1,11 @@
+// Function in Javascript
+
+// Q1: What is Function Declaration
+
+// Declare the function 
+function square(num) {
+    return(num * num);
+};
+
+// Call the Function and console the value
+console.log(square(10)); // 100
